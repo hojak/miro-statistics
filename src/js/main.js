49c7be07e7824856429c749e7ee83c9e87157281
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-06-08 13:53:30 +0200

    moved all js code out of index.html

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,39 @@
 const MiroTextHelper = require("./MiroTextHelper");
+
+
+const icon = '<circle cx="12" cy="12" r="9" fill="none" fill-rule="evenodd" stroke="currentColor" stroke-width="2"></circle>';
+
+miro.onReady(() => {
+  miro.initialize({ 
+    extensionPoints: {
+      bottomBar: async () => {
+        const authorized = await miro.isAuthorized()
+        
+        if (authorized) {
+          return {
+            title: 'Authorized example',
+            svgIcon: icon,
+            onClick: sayHi,
+          }
+        } else {
+          return miro.authorize({
+            'response_type': 'code',
+          }).then( () => {
+            return {
+              title: 'Authorized example',
+              svgIcon: icon,
+              onClick: sayHi,
+            }
+          });
+        }
+      },
+    },
+  })
+})
+
+
+
+function sayHi() {
+  miro.board.widgets.create({type: 'sticker', text: "whatever we want"})
+  alert('Hi!')
+}
