c16264bf84f7618db0959eba7e376e96ee3be04d
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-06-15 15:15:30 +0200

    added simple point class

---
+++ src/js/Point.js
@@ -0,0 +1,15 @@
+class Point {
+    constructor ( x, y ) {
+        if ( isNaN ( x ) || isNaN ( y) || x == null || y == null ) {
+            throw new TypeError ( "not a number");
+        }
+
+        this.x = x;
+        this.y = y;
+    }
+
+    get X() { return this.x }
+    get Y() { return this.y }
+}
+
+module.exports = Point;
