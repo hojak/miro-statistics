d8638df30e28477891e267e2bc3cd23ff995195d
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-06-22 18:58:03 +0200

    chore: fix linter for specific line

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@
 const MiroKanbanController = require('./MiroKanbanController')
 
 window.onload = function () {
-  // linter hint: without "new", this line fails in the browser...
+  // without "new", this line fails in the browser...
+  // eslint-disable-next-line no-new
   new MiroKanbanController(miro)
 }
