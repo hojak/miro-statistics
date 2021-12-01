6e19b030509327eb732be52bfb293c6251eb81cf
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-12-01 18:13:14 +0100

    fix: more special characters possible (', ", ...)

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -7,8 +7,9 @@ const controller = new MiroKanbanController(miro)
 window.onload = function () {
   document.getElementById('button_export_csv').onclick = function () {
     controller.getCsvData().then(data => {
+      const universalBOM = "\uFEFF";
       const csvFile =
-      'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURI(data);
+      'data:text/csv; charset=utf-8,' + encodeURIComponent (universalBOM + data);
       window.open(csvFile)
     })
   }
