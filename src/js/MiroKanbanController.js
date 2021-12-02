e3d4ed01346fec0577fd580dd71339e1476de4f0
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2021-12-02 12:38:25 +0100

    refactor: devide getCsvData

--- src/js/MiroKanbanController.js
+++ src/js/MiroKanbanController.js
@@ -18,22 +18,35 @@ class MiroKanbanController {
     }])
   }
 
+  async getAllCards () {
+    return await this.miro.board.widgets.get({ type: 'CARD' })
+  }
+
+  getEventlistOfCards ( miroCardData ) {
+    return miroCardData.map(card => MiroTextHelper.extractEventList(card.description, card.id))
+      .flatMap(eventList => eventList.getItems())
+      .sort((eventA, eventB) => eventA.getTimestamp() - eventB.getTimestamp())
+  }
+
   async getCsvData () {
     const $this = this
-    return await $this.miro.board.widgets.get({ type: 'CARD' })
+    return await $this.getAllCards ()
       .then(function (data) {
         return 'cardId;new state;timestamp;\n' +
-          data.map(card => MiroTextHelper.extractEventList(card.description, card.id))
-            .flatMap(eventList => eventList.getItems())
-            .sort((eventA, eventB) => eventA.getTimestamp() - eventB.getTimestamp())
+          $this.getEventlistOfCards ( data )
             .map(event => event.toCSV())
             .join('') +
           '\n\n\n\ncardId;title;\n' +
           data.map(card => card.id + ';' + $this.removeHtml(card.title) + ';'
           ).join('\n')
-      }
-      )
+      })
   }
+
+  async getChronologicalEventList () {
+    const $this = this
+    return await $this.getAllCards().then ( $this.getEventlistOfCards );
+  }
+
 
   removeHtml (text) {
     return text
