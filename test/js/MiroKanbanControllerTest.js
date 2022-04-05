057fbcd2eff4a8af222499d11acb852d22dd9946
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2022-04-05 18:25:48 +0200

    test: add test for expected behaviour
    getting the complete event list leads to an error when a card contains a neighboring conflict. This is a testcase for this problem.

---
+++ test/js/MiroKanbanControllerTest.js
@@ -0,0 +1,34 @@
+
+const { expect, assert } = require('chai')
+const Point = require('../../src/js/Point')
+const MiroKanbanController = require('../../src/js/MiroKanbanController')
+const { ERROR_NEIGHBOR_CONFLICT } = require('../../src/js/ItemEventList')
+const { START_EVENT_LIST, END_EVENT_LIST } = require('../../src/js/MiroTextHelper')
+
+describe('MiroKanbanController', function () {
+
+    let miroMock = {}
+    const miroKanbanController = new MiroKanbanController(miroMock)
+
+    describe ( 'getAllCardEventlists', function () {
+        it('has to return an empty list', function () {
+            const cardData = []
+            expect ( miroKanbanController.getAllCardEventlists (cardData) ).to.be.empty
+        })
+
+        it('must not propagate a neighbor conflict', function () {
+            const cardData = [
+                {
+                    name: 'errorprone card',
+                    description:  START_EVENT_LIST + '<p>work: 2021-07-10 10:00,</p><p>work: 2021-08-10 11:00,</p><p>done: 2021-08-10 13:00</p>' + END_EVENT_LIST,
+                    id: 123
+                },
+            ]
+
+            let collectedStrings = "";
+            console.log = function ( s ) { collectedStrings += s }
+            expect ( miroKanbanController.getAllCardEventlists (cardData)).to.deep.equal ( [null]) 
+            expect ( collectedStrings ).to.contain (cardData[0].name)
+        })
+    })
+})
