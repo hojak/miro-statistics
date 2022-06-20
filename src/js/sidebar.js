2882d9820216970e062e66dfb2fb0c53bc798e7d
Holger Janßen-Kroll <holger.janssen-kroll@aleri.de>
2022-06-20 13:54:08 +0200

    refactor: extract inline functions from onload

--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -22,73 +22,75 @@ window.onload = function () {
 
   // BUG: miro.board sometimes not ready here leading to an empty select field
   initializeCardStatusSelect()
+}
 
-  function initializeCardStatusSelect () {
-    controller.getAllEventStates().then(listOfStates => {
-      const select = document.getElementById('card_status')
-      select.innerHTML = ''
 
-      listOfStates.forEach(item => {
-        const option = document.createElement('option')
-        option.innerHTML = item
-        select.appendChild(option)
-      })
+function initializeCardStatusSelect () {
+  controller.getAllEventStates().then(listOfStates => {
+    const select = document.getElementById('card_status')
+    select.innerHTML = ''
+
+    listOfStates.forEach(item => {
+      const option = document.createElement('option')
+      option.innerHTML = item
+      select.appendChild(option)
     })
-  }
+  })
+}
 
-  function addShowCardsOfStatusClickHandler () {
-    document.getElementById('button_show_cards_in_selected_status').onclick = function () {
-      const selectedStatus = document.getElementById('card_status').value
+function addShowCardsOfStatusClickHandler () {
+  document.getElementById('button_show_cards_in_selected_status').onclick = function () {
+    const selectedStatus = document.getElementById('card_status').value
 
-      controller.getAllCards()
-        .then(listOfCards => listOfCards.filter(miroCard => isCardInSelectedStatus(miroCard, selectedStatus)))
-        .then(showListOfCards)
-    }
-  }
-
-  function addShowLfdClickHandler () {
-    document.getElementById('button_show_ltd').onclick = function () {
-      controller.getAllCards().then(showLtdForEventList)
-    }
-  }
-
-  function addShowCfdClickHandler () {
-    document.getElementById('button_show_cfd').onclick = function () {
-      const columnDefinitions = document.getElementById('cfd_groups').value.split('\n').map(entry => entry.split(','))
-
-      const filterBefore = getBeforeTimestamp()
-      const filterAfter = getAfterTimestamp()
-
-      controller.getChronologicalEventList().then(
-        data => {
-          const cfdAnalyzer = new CfdAnalyzer(columnDefinitions, data)
-          if (filterBefore) {
-            data = data.filter(item => item.getTimestamp() >= filterBefore)
-            cfdAnalyzer.startAtDate = filterBefore
-          }
-          if (filterAfter) {
-            data = data.filter(item => item.getTimestamp() <= filterAfter)
-            cfdAnalyzer.endAtDate = filterAfter
-          }
-
-          const cfdData = cfdAnalyzer.getCfdData()
-
-          showCfd(columnDefinitions, cfdAnalyzer.getTimestampsOfDaylies().map(ts => new Date(ts).toLocaleString()), cfdData)
-        }
-      )
-    }
-  }
-
-  function addExportCsvClickHandler () {
-    document.getElementById('button_export_csv').onclick = function () {
-      controller.getCsvData().then(data => {
-        const universalBOM = '\uFEFF'
-        const csvFile = 'data:text/csv; charset=utf-8,' + encodeURIComponent(universalBOM + data)
-        window.open(csvFile)
-      })
-    }
+    controller.getAllCards()
+      .then(listOfCards => listOfCards.filter(miroCard => isCardInSelectedStatus(miroCard, selectedStatus)))
+      .then(showListOfCards)
   }
 }
+
+function addShowLfdClickHandler () {
+  document.getElementById('button_show_ltd').onclick = function () {
+    controller.getAllCards().then(showLtdForEventList)
+  }
+}
+
+function addShowCfdClickHandler () {
+  document.getElementById('button_show_cfd').onclick = function () {
+    const columnDefinitions = document.getElementById('cfd_groups').value.split('\n').map(entry => entry.split(','))
+
+    const filterBefore = getBeforeTimestamp()
+    const filterAfter = getAfterTimestamp()
+
+    controller.getChronologicalEventList().then(
+      data => {
+        const cfdAnalyzer = new CfdAnalyzer(columnDefinitions, data)
+        if (filterBefore) {
+          data = data.filter(item => item.getTimestamp() >= filterBefore)
+          cfdAnalyzer.startAtDate = filterBefore
+        }
+        if (filterAfter) {
+          data = data.filter(item => item.getTimestamp() <= filterAfter)
+          cfdAnalyzer.endAtDate = filterAfter
+        }
+
+        const cfdData = cfdAnalyzer.getCfdData()
+
+        showCfd(columnDefinitions, cfdAnalyzer.getTimestampsOfDaylies().map(ts => new Date(ts).toLocaleString()), cfdData)
+      }
+    )
+  }
+}
+
+function addExportCsvClickHandler () {
+  document.getElementById('button_export_csv').onclick = function () {
+    controller.getCsvData().then(data => {
+      const universalBOM = '\uFEFF'
+      const csvFile = 'data:text/csv; charset=utf-8,' + encodeURIComponent(universalBOM + data)
+      window.open(csvFile)
+    })
+  }
+}
+
 
 function getTimstampFromInput (htmlId) {
   const input = document.getElementById(htmlId).value
